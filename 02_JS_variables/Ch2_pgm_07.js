c679439a4a402d2bbf06435123839586b6a1d7a8
janani <janani.raghupriya@gmail.com>
2024-07-09 19:16:07 +0530

    update

--- 02_JS_variables/Ch2_pgm_07.js
+++ 02_JS_variables/Ch2_pgm_07.js
@@ -2,8 +2,11 @@
 
 var playerName = "Kandra";
 var locationName = "The Dungeon of Doom";
+var health = "50";
+var message;
+message = playerName + " has health " + health + " and is in " + locationName;
 
-console.log(playerName + " is in " + locationName);
+console.log(message);
 
 
 
