1a596f49fd44d822f6853298e8e6367066e5bfec
janani <janani.raghupriya@gmail.com>
2024-07-09 15:54:07 +0530

    update

--- 02_JS_variables/Ch2_pgm_05.js
+++ 02_JS_variables/Ch2_pgm_05.js
@@ -2,10 +2,12 @@
 
 var message;
 
-message = "Hello World!";
+message = "Hello"+"World!";
 console.log(message);
 
-message = 'Congratulations! Your tweet has won a prize...';
+message = "Congratulations!"+" Your tweet has won"+" a prize...";
+console.log(message);
+message = "welcome....."+"world";
 console.log(message);
 
 
