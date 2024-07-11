64cd00f717bd38b39ce2c2bcc50fc04351e3566e
janani <janani.raghupriya@gmail.com>
2024-07-11 14:34:57 +0530

    update

--- 06_JS_Return_values/Ch6_pgm_10.js
+++ 06_JS_Return_values/Ch6_pgm_10.js
@@ -11,12 +11,14 @@ var getPlayerHealth = function (playerName, playerHealth) {
 var getPlayerPlace = function (playerName, playerPlace) {
     return playerName + " is in " + playerPlace;
 };
-
+var getPlayerItems = function (playerName, playerItems) {
+    return playerName + " has the following items: " + playerItems;
+};
 var getBorder = function () {
     return "================================";
 };
 
-var getPlayerInfo = function (playerName, playerPlace, playerHealth) {
+var getPlayerInfo = function (playerName, playerPlace, playerHealth,getPlayerItems) {
     var playerInfo;
 
     playerInfo = "\n" + getPlayerName(playerName);
@@ -32,17 +34,19 @@ var getPlayerInfo = function (playerName, playerPlace, playerHealth) {
 var player1 = {
     name: "Kandra",
     place: "The Dungeon of Doom",
-    health: 50
+    health: 50,
+    items: "a rusty key, a piece of cheese"
 };
 
 var player2 = {
     name: "Dax",
     place: "The Old Library",
-    health: 40
+    health: 40,
+    items: "an ancient book, a candle"
 };
 
-console.log(getPlayerInfo(player1.name, player1.place, player1.health));
-console.log(getPlayerInfo(player2.name, player2.place, player2.health));
+console.log(getPlayerInfo(player1.name, player1.place, player1.health,player1.items));
+console.log(getPlayerInfo(player2.name, player2.place, player2.health,player1.items));
 
 
 
