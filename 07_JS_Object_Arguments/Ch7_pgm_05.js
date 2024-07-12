4c78ae3b2652d4880c5dcd621f247b17aeb88521
janani <janani.raghupriya@gmail.com>
2024-07-12 13:04:34 +0530

    update

--- 07_JS_Object_Arguments/Ch7_pgm_05.js
+++ 07_JS_Object_Arguments/Ch7_pgm_05.js
@@ -7,8 +7,27 @@ var showSmaller = function (num1, num2) {
     console.log(smaller + " is smaller than " + larger);
 };
 
+var showLarger = function (num1, num2){
+    var smaller = Math.min(num1,num2);
+    var larger = Math.max(num1,num2);
+    console.log(larger + "is larger than" + smaller);
+};
+var showSmallest = function(num1,num2,num3){
+    var smallest = Math.min(num1,num2,num3);
+    console.log(smallest + "is the smallest of" + num1 + "," + num2 + ", and" + num3);
+}
 showSmaller(12, 3);
 showSmaller(-10, 3);
+showSmaller(7,7);
+showSmaller(5.5,5.4);
+showLarger(12,3);
+showLarger(-10,3);
+showLarger(7,7);
+showLarger(5.5,5.4);
+showSmallest(12,3,-5);
+showSmallest(-10,3,7);
+showSmallest(7,7,7);
+showSmallest(5.5,5.4,9.6);
 
 
 
